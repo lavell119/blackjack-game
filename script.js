ff87ad493c923213b92cd888cf36d3dcda18c5cf
Lee <leewhiz119@gmail.com>
2022-08-29 22:29:29 -0400

    added turn-changing functionality

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let card= document.querySelectorAll('.card')
+let player= document.querySelectorAll('.player')
+let seat= document.querySelectorAll('.seat')
+let testButton = document.querySelector('.test-button')
+
+
 
 fetch("./deck.json")
 .then(response=>{
@@ -10,3 +15,27 @@ fetch("./deck.json")
 card[0].style.backgroundImage="url('deck-images/king_of_hearts.png')"
 
 card[1].style.backgroundImage="url('deck-images/ace_of_spades.png')"
+
+let startGame=()=>{
+    player.forEach(player=()=>{
+        score=0
+        console.log(score)
+    }
+)
+seat[0].classList.add('active')
+}
+  
+startGame()
+
+testButton.addEventListener('click', ()=>{
+    nextTurn()
+})
+
+let i =0
+let nextTurn =()=>{
+    seat[i].classList.remove('active')
+    seat[i+1].classList.add('active')
+    i=i+1
+}
+
+
