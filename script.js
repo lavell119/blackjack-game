c985d2938dac1abe15535b4d5d813cbd0549535d
Lee <leewhiz119@gmail.com>
2022-08-30 00:07:15 -0400

    formatted card margin and added bold to chip count

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let card= document.querySelectorAll('.card')
 let player= document.querySelectorAll('.player')
 let seat= document.querySelectorAll('.seat')
 let testButton = document.querySelector('.test-button')
+let stayButton= document.querySelectorAll('.stay-btn')
+let btnContainer= document.querySelectorAll('.btn-container')
+let hand= document.querySelectorAll('.hand')
+let hitButton=document.querySelectorAll('.hit-btn')
 
 
 
@@ -12,9 +16,6 @@ fetch("./deck.json")
 .then(data=>deck=data)
 .then(deck=>console.log(deck))   
 
-card[0].style.backgroundImage="url('deck-images/king_of_hearts.png')"
-
-card[1].style.backgroundImage="url('deck-images/ace_of_spades.png')"
 
 let startGame=()=>{
     player.forEach(player=()=>{
@@ -24,17 +25,34 @@ let startGame=()=>{
 )
 seat[0].classList.add('active')
 }
-  
 
-testButton.addEventListener('click', ()=>{
-    nextTurn()
-})
+hitButton.forEach (button=> 
+    button.addEventListener('click', ()=>{
+    let e = Math.floor(Math.random() * 52)
+    let drawnCard=(deck[e])
+    console.log(drawnCard.name)
+    let newCard=document.createElement('div')
+    newCard.classList.add('card')
+    newCard.style.backgroundImage="url('deck-images/"+drawnCard.image+".png')"
+    hand[i].appendChild(newCard)
+    })
+    )
+
+stayButton.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        nextTurn()
+    }
+)}
+)
 
 let i =0
 let nextTurn =()=>{
     seat[i].classList.remove('active')
     seat[i+1].classList.add('active')
+    btnContainer[i].classList.add('hide')
+    btnContainer[i+1].classList.remove('hide')
     i=i+1
+
 }
 
 
