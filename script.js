60a10c65af5b47196080155173341c320b4f00ca
Lee <leewhiz119@gmail.com>
2022-08-30 23:27:22 -0400

    added winner div

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let stayButton= document.querySelectorAll('.stay-btn')
 let btnContainer= document.querySelectorAll('.btn-container')
 let hand= document.querySelectorAll('.hand')
 let hitButton=document.querySelectorAll('.hit-btn')
+let scoreElement= document.querySelectorAll('.score')
+let winner= document.querySelector('.winner')
+let score=0
 
 
 
@@ -26,16 +29,20 @@ let startGame=()=>{
 seat[0].classList.add('active')
 }
 
-hitButton.forEach (button=> 
+hitButton.forEach (button=> {
+    let score =0
     button.addEventListener('click', ()=>{
     let e = Math.floor(Math.random() * 52)
     let drawnCard=(deck[e])
     console.log(drawnCard.name)
+    score=score+drawnCard.value
     let newCard=document.createElement('div')
     newCard.classList.add('card')
     newCard.style.backgroundImage="url('deck-images/"+drawnCard.image+".png')"
     hand[i].appendChild(newCard)
+    scoreElement[i].innerText=score
     })
+}
     )
 
 stayButton.forEach((button)=>{
@@ -47,12 +54,15 @@ stayButton.forEach((button)=>{
 
 let i =0
 let nextTurn =()=>{
+    if (i===3) {
+        winner.classList.remove('hide')
+    } else {
     seat[i].classList.remove('active')
     seat[i+1].classList.add('active')
     btnContainer[i].classList.add('hide')
     btnContainer[i+1].classList.remove('hide')
     i=i+1
-
+    }
 }
 
 
