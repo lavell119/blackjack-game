583ba9bdcb22ff334bfa6fca95f1b4e64509a94f
Lee <leewhiz119@gmail.com>
2022-09-26 12:08:45 -0400

    added create account form submit functionaity

--- login.js
+++ login.js
@@ -1,6 +1,19 @@
 let createAccountPopup=document.querySelector('.create-account-popup')
 let createAccountButton=document.querySelector('.create-account-btn')
+let createAccountForm =document.querySelector('.create-account-form')
 
 createAccountButton.addEventListener('click', ()=>{
     createAccountPopup.classList.remove('hide')
 })
+
+let nickNameInput=document.querySelector('.nick-name')
+
+
+createAccountForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log('asffa')
+    let nickName=nickNameInput.value
+    console.log(nickName)
+
+}
+)
